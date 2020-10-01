3f64585f3a0537c70aaf1d4aa4779bc2333b6b20
deshraj12045 <72187239+deshraj12045@users.noreply.github.com>
2020-10-01 12:05:08 +0530

    Add files via upload

---
+++ up.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t,n,x,a;
+
+
+    cin>>t;
+
+    int g2[t];
+
+    for(int j=0;j<t;j++){
+        int b=0;
+        int z=0;
+        cin>>n>>x;
+
+        int g1[n];
+
+        for(int i=0;i<n;i++){
+            cin>>g1[i];
+        }
+
+        sort(g1,g1+n);
+
+        
+    }
+
+    for(int i=0;i<t;i++){
+        cout<<"Case #"<<i+1<<": "<<g2[0]<<endl;
+    }
+
+    return 0;
+
+}
